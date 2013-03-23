ad693bd1696a9897f5e94b093d207c83e4eb72e9
paulo <vagnerpso@gmail.com>
2013-03-23 17:42:59 -0300

    Atualizando estrutura para a versão de ruby mais atualizada

---
+++ core/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Core::Application.config.secret_token = '03f02f1dbfffb3e30cec2ff43adce8eccb55463c87753520f24300d3df167c57236c69292aa8777ed3cd6c74b02905ccdefe35b9efd87675c8997a34a949b5ea'
