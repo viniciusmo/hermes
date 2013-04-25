93fa00048c95fff7299b960a8f1a70d7b8339c2e
viniciusmo <vinicius.moises.oliveira@gmail.com>
2013-04-25 20:53:14 -0300

    Update/List/Create usuario terminado

--- core/app/controllers/users_controller.rb
+++ core/app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
         @result.status = false
     end
     respond_to do |format|
-      format.any { render :json => @result }
+      format.any { render :json => @users.to_json(:except => :password)}
     end
   end
 
@@ -27,47 +27,54 @@ class UsersController < ApplicationController
         @result.message = "Usuario nao encontrado"
         @result.status = false
     end
-
     respond_to do |format|
       format.any { render :json => @result }
     end
   end
 
-
-  # POST /users
-  # POST /users.json
   def create
+    @result = Result.new
     @user = User.new
-    @user.
+    @user.name = params[:name]
+    @user.email = params[:email]
+    @user.password = params[:password]
+    if @user.save
+        @result.message = "Sucesso"
+        @result.status = true
+    else
+        @result.message = "Ocorreu um erro"
+        @result.status = false
+        @result.data = @user.errors
+    end
     respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+      format.any { render :json => @result }
     end
   end
-  
-  def update
-    @user = User.find(params[:id])
 
+  def update
+    @result = Result.new
+    begin
+        @user = User.find(params[:id])
+        if @user.update_attributes name: params[:name], email: params[:email], password: params[:password] then
+            @result.message = "Sucesso"
+            @result.status = true
+        else
+            @result.message = "Nao foi possivel realizar o update do usuario"
+            @result.status = false
+            @result.data = @user.errors
+        end
+    rescue ActiveRecord::RecordNotFound => e
+        @result.message = "Usuario nao encontrado"
+        @result.status = false
+    end
     respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+      format.any { render :json => @result }
     end
   end
 
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-
     respond_to do |format|
       format.html { redirect_to users_url }
       format.json { head :no_content }
--- core/app/models/user.rb
+++ core/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :nome, :senha
+  attr_accessible :email, :name, :password
+  validates :email,presence: true, uniqueness: true
+  validates :name,presence: true
+  validates :password, presence: true,length: {minimum: 3}
 end
