e811133d5e17e47be99ac836ff22d7a294a423d0
viniciusmo <vinicus.moises.oliveira@gmail.com>
2013-04-28 14:16:51 -0300

    Criando routes nested para facilitar o acesso dos servicos

--- core/app/controllers/board_controller.rb
+++ core/app/controllers/board_controller.rb
@@ -1,8 +1,18 @@
 class BoardController < ApplicationController
   
   def index
-  	respond_to do |format|
-      format.any { render :json => "ok" }
+    @boards = Board.where(["user_id = ?",params[:user_id]])
+    @result = Result.new
+    if @boards.count > 0 then
+        @result.message = t(:sucess)
+        @result.status = true
+        @result.data = @boards
+    else
+        @result.message = t(:error)
+        @result.status = false
+    end
+    respond_to do |format|
+      format.any { render :json => @result }
     end
   end
 
--- core/db/schema.rb
+++ core/db/schema.rb
@@ -14,20 +14,16 @@
 ActiveRecord::Schema.define(:version => 20130428125525) do
 
   create_table "boards", :force => true do |t|
-    t.string   "name"
-    t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string  "name"
+    t.integer "user_id"
   end
 
   add_index "boards", ["user_id"], :name => "index_boards_on_user_id"
 
   create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string "name"
+    t.string "email"
+    t.string "password"
   end
 
 end
