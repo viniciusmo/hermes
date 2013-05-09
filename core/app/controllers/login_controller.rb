61dc5e7eeb851416f2d267473bfcaf64ac3c0ba9
viniciusmo <vinicius.moises.oliveira@gmail.com>
2013-05-09 19:18:13 -0300

    Validação básica login

--- core/app/controllers/login_controller.rb
+++ core/app/controllers/login_controller.rb
@@ -1,6 +1,19 @@
+require 'digest/md5'
+
 class LoginController < ApplicationController
 
 	def index
-		
+        
 	end
+
+	def logar
+        @user = User.find_by_email_and_password(params[:user][:email],
+        										Digest::MD5.hexdigest(params[:user][:password]))
+        if @user
+        	render :text => "ok"
+        else
+        	render :text => "nao"
+        end
+	end
+
 end
