b46262622432a3815cdf372ad9bbd516d26e2ba0
viniciusmo <vinicius.moises.oliveira@gmail.com>
2013-05-28 15:20:10 -0300

    Simplificando a autenticação

--- core/app/controllers/application_controller.rb
+++ core/app/controllers/application_controller.rb
@@ -2,11 +2,13 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery
 
 	def authenticate_user
-		unless session[:user]
-			flash[:error] = t(:error_restrict_area)
-			redirect_to(:controller => :authenticate, :action => :index)
-		else
-			@current_user = session[:user]
-		end
+		@user = User.find_by_email_and_password(params[:user][:email],
+        										Digest::MD5.hexdigest(params[:user][:password]))
+        unless @user
+        	@result = Result.new ({:message => t(:sucess),:status => false})
+        	respond_to do |format|
+		      format.any { render :json => @result }
+		    end
+        end
 	end
 end
