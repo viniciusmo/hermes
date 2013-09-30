53346a47712e98b4e90082f788e3e3580e4d60e0
viniciusmo <vinicius.moises.oliveira@gmail.com>
2013-09-29 21:12:30 -0300

    Criando as ações para 'EU'

--- mobile/android/hermes/gen/com/hermes/R.java
+++ mobile/android/hermes/gen/com/hermes/R.java
@@ -64,13 +64,16 @@ public final class R {
         public static final int activity_texto_audio_btn_falar=0x7f050004;
         public static final int activity_texto_audio_hint_text=0x7f050005;
         public static final int app_name=0x7f050000;
+        public static final int estou=0x7f05000e;
         public static final int eu=0x7f05000c;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
+        public static final int quero=0x7f05000f;
         public static final int title_activity_imagesound=0x7f05000a;
         public static final int title_activity_menu_principal=0x7f050006;
         public static final int title_activity_texto_audio=0x7f050003;
         public static final int voce=0x7f05000d;
+        public static final int vou=0x7f050010;
     }
     public static final class style {
         /** 
--- mobile/android/hermes/src/com/hermes/activity/VerbsMe.java
+++ mobile/android/hermes/src/com/hermes/activity/VerbsMe.java
@@ -1,10 +1,26 @@
 package com.hermes.activity;
 
+import android.view.View;
+
 import com.hermes.reflection.AnnotatedActivity;
 import com.hermes.reflection.Layout;
+import com.hermes.tools.SoundTools;
 import com.hermes.R;
 
 @Layout(R.layout.activity_verbs_me)
 public class VerbsMe extends AnnotatedActivity {
 
+	public void want(View v) {
+		SoundTools.playFileFromAssets("quero.mp3");
+	}
+
+	public void am(View v) {
+		SoundTools.playFileFromAssets("estou.mp3");
+
+	}
+
+	public void go(View v) {
+		SoundTools.playFileFromAssets("vou.mp3");
+
+	}
 }
