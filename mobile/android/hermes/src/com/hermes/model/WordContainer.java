3fa17cb8ae5eb219236b16ddba70d04001c574e1
viniciusmo <viniciusmo@viniciusmos-MacBook-Pro.local>
2013-11-10 21:24:36 -0200

    WordContainer

---
+++ mobile/android/hermes/src/com/hermes/model/WordContainer.java
@@ -0,0 +1,9 @@
+package com.hermes.model;
+
+import java.util.List;
+
+public class WordContainer {
+
+	public List<Word> words;
+
+}
--- mobile/android/hermes/src/com/hermes/sync/SyncWords.java
+++ mobile/android/hermes/src/com/hermes/sync/SyncWords.java
@@ -1,10 +1,20 @@
 package com.hermes.sync;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.hermes.R;
+import com.hermes.tools.ApplicationContext;
+import com.hermes.tools.WebClient;
+
 public class SyncWords implements Synceable {
 
 	@Override
 	public void doSync() {
-
+		String url = ApplicationContext.res().getString(
+				R.string.url_service_video_libras);
+		String result = WebClient.getContent(url);
+		Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation()
+				.create();
 	}
 
 }
