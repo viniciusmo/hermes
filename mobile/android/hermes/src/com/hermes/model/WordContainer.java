6cc0990923f4bd1de9b693b6442dd56dd1fb7dee
viniciusmo <viniciusmo@viniciusmos-MacBook-Pro.local>
2013-11-10 21:37:33 -0200

    WordContainer

--- mobile/android/hermes/src/com/hermes/model/WordContainer.java
+++ mobile/android/hermes/src/com/hermes/model/WordContainer.java
@@ -2,8 +2,19 @@ package com.hermes.model;
 
 import java.util.List;
 
+import com.google.gson.annotations.Expose;
+
 public class WordContainer {
 
+	public List<Word> getWords() {
+		return words;
+	}
+
+	public void setWords(List<Word> words) {
+		this.words = words;
+	}
+
+	@Expose
 	public List<Word> words;
 
 }
--- mobile/android/hermes/src/com/hermes/sync/SyncWords.java
+++ mobile/android/hermes/src/com/hermes/sync/SyncWords.java
@@ -3,7 +3,11 @@ package com.hermes.sync;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.hermes.R;
+import com.hermes.model.Word;
+import com.hermes.model.WordContainer;
+import com.hermes.model.dao.WordDao;
 import com.hermes.tools.ApplicationContext;
+import com.hermes.tools.Log;
 import com.hermes.tools.WebClient;
 
 public class SyncWords implements Synceable {
@@ -13,8 +17,21 @@ public class SyncWords implements Synceable {
 		String url = ApplicationContext.res().getString(
 				R.string.url_service_video_libras);
 		String result = WebClient.getContent(url);
+		Log.i("Carregando de " + url);
+
 		Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation()
 				.create();
+		WordContainer wordContainer = gson
+				.fromJson(result, WordContainer.class);
+		WordDao wordDao = WordDao.create();
+
+		if (wordDao.loadAll().isEmpty()) {
+			Log.i("Carregando words");
+			for (Word word : wordContainer.getWords()) {
+				wordDao.insert(word);
+			}
+		}
+		wordDao.getDatabase().close();
 	}
 
 }
